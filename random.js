01a4230b928583caaf2b3827d34d119867f3659e
hmboy12 <126798296+hmboy12@users.noreply.github.com>
2023-12-02 11:46:23 +0100

    Add files via upload

---
+++ random.js
@@ -0,0 +1,22 @@
+//The function that generates the random number from the user interval
+let generateButton = document.getElementById("generateRandomNumberButton");
+generateButton.addEventListener('click', () => generateRandomNumberFromUserInterval());
+
+function generateRandomNumberFromUserInterval(){
+    let from = document.getElementById("randomNumberFrom");
+    let to = document.getElementById("randomNumberTo");
+    if(from.value>=to.value){
+        alert("Invalid interval");
+    } else {
+        let randomNumber = getRandomNumber(from.value, to.value);
+        let showResult = document.getElementById("randomNumberResult");
+        showResult.innerText = "Result: "+ randomNumber.toString();
+    }
+
+}
+
+function getRandomNumber(min, max){
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
